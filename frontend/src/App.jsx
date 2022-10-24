583df9e94c3a682f6d9fd6f69704c7e2c1b5e0d0
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-10-24 10:54:21 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Landingpage from "./pages/landingpage"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Landingpage />
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/landingpage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../css/landingpage.css"
+
+function Landingpage() {
+  return (
+    <div className="landing-page">
+      <div className="nav"></div>
+    </div>
+  )
+}
+
+export default Landingpage
