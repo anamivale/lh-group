28414bd655342bcd459cc07f277a2829bb4e24a4
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-11-09 09:39:05 +0300

    styling

---
+++ frontend/src/admin/booking.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function booking() {
+  return <div>booking</div>
+}
+
+export default booking
--- frontend/src/pages/dishes.js
+++ frontend/src/pages/dishes.js
@@ -1,9 +1,21 @@
 import React from "react"
+import Nav from "./nav"
+import Dish1 from "../images/dish1.jpg"
+import Dish2 from "../images/dish2.jpg"
+import Dish3 from "../images/mandazi.jpg"
+import Dish4 from "../images/nyama.jpg"
 
 function Dishes() {
   return (
     <div>
-      <img src="" alt="" />
+      <Nav />
+      <img src={Dish1} alt="" />
+      <br />
+      <img src={Dish2} alt="" />
+      <br />
+      <img src={Dish3} alt="" />
+      <br />
+      <img src={Dish4} alt="" />
     </div>
   )
 }
